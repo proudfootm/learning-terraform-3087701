c54fd63e2be35be35172411443cf6f4a25687fe5
proudfootm <marc74uk@gmail.com>
2024-09-10 15:06:55 +0100

    Updated security group using module

--- main.tf
+++ main.tf
@@ -22,14 +22,14 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  vpc_security_group_ids = [module.blog.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
 }
 
-module "blog_sg" {
+module "blog" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
   name = "blog_new"
@@ -39,43 +39,4 @@ module "blog_sg" {
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"] 
-}
-
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "allow https/http in, allow all out"
-
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "blog_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-
-resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = -1
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
 }
