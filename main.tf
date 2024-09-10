c01c13b63cd5e92a6a5dae24acf485105e7aec4a
proudfootm <marc74uk@gmail.com>
2024-09-10 15:15:39 +0100

    secgrp name change

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "blog" {
 module "blog" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
-  name = "blog_new"
+  name = "blog"
 
   vpc_id = data.aws_vpc.default.id
   ingress_rules = ["http-80-tcp","https-443-tcp"]
